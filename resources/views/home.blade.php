443ef76d7aa24fe112adc2b38d3b0927323d7dbb
MichalWolnyDev <mwolny98@interia.pl>
2022-12-10 13:51:37 +0100

    zmiany

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,71 @@
 
 @section('content')
 <div class="container">
+    WIDOK DLA ADMINA
     <div class="row shadow">
-    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
-  </li>
-</ul>
-<div class="tab-content" id="pills-tabContent">
-  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">1</div>
-  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">2</div>
-  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">3</div>
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="pills-addteacher-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-addteacher" type="button" role="tab" aria-controls="pills-addteacher"
+                    aria-selected="true">Dodaj nauczyciela</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="pills-addstudent-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-addstudent" type="button" role="tab" aria-controls="pills-addstudent"
+                    aria-selected="false">Dodaj ucznia</button>
+            </li>
+
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-addteacher" role="tabpanel"
+                aria-labelledby="pills-addteacher-tab">rzeczy z dodawania nauczyciela
+            </div>
+            <div class="tab-pane fade" id="pills-addstudent" role="tabpanel" aria-labelledby="pills-addstudent-tab">
+                rzeczy z dodawnaia ucznia</div>
+        </div>
+    </div>
 </div>
+<div class="container">
+    WIDOK DLA NAUCZYCIELA
+    <div class="row shadow">
+
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item dropdown">
+                <a class="nav-link active dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                    aria-expanded="false">Klasa</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">1a</a></li>
+                    <li><a class="dropdown-item" href="#">1b</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-schoolclass" role="tabpanel"
+                aria-labelledby="pills-schoolclass-tab">rzeczy nauczyciela
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    WIDOK DLA UCZNIA
+    <div class="row shadow">
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="pills-grades-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-grades" type="button" role="tab" aria-controls="pills-grades"
+                    aria-selected="true">Oceny</button>
+            </li>
+
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-grades" role="tabpanel" aria-labelledby="pills-grades-tab">
+                oceny
+            </div>
+        </div>
     </div>
 </div>
 
